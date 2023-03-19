453c9c2e0ac8f99911d09332e15d64a26150fd88
wjh203 <0203hjw@gmail.com>
2023-03-19 21:28:20 +0900

    일부 변경

--- README.md
+++ README.md
@@ -3,7 +3,6 @@ BaseModel
 ├── model
 │   ├── .gitattributes
 │   ├── config.json
-│   ├── pytorch_model.bin
 │   ├── special_tokens_map.json
 │   ├── tokenizer_config.json
 │   └── tokenizer.json
@@ -14,7 +13,7 @@ BaseModel
 │   └── project.py
 └── test.ipynb
 ```
-- model: 요약모델과 토크나이저의 가중치 파일이 있습니다.
+- model: 요약모델 및 토크나이저의 가중치 파일이 있습니다.
 - result: project.py로 요약한 결과가 있습니다.
 - text: result 요약에 쓰인 텍스트들이 있습니다.
 - utils: 요약 모델을 불러오는 모듈인 project 모듈이 있습니다. 모듈 안의 Summary 클래스에는 텍스트 파일을 여는 open_txt와 텍스트를 전처리하는 preprocess, 요약 예측을 하는 inference 메서드가 있습니다.
@@ -22,8 +21,8 @@ BaseModel
 from utils.project import Summary #project.py에 구현된 Summary 클래스를 불러옵니다. 
 
 summary = Summary()
-text = summary.open_txt('파일 경로') #텍스트 파일을 불러옵니다.
-preprocessed_text = summary.preprocess(text) #전처리한 텍스트를 반환합니다.
-summary.inference(preprocessed_text) #요약한 텍스트를 반환합니다.
+summary.stack_txt('파일 경로') #텍스트 파일 내 텍스트를 리스트에 옮깁니다. summary.text_lst를 통해 리스트를 보실 수 있습니다.
+summary.preprocess() #리스트 내 텍스트를 전처리합니다. 마찬가지로 summary.text_lst를 통해 리스트 내 전처리한 텍스트를 보실 수 있습니다.
+summary_text = summary.inference() #리스트 내 각 텍스트를 요약하고 그 결과를 리스트로 반환합니다.
 ```
 - test.ipynb: 해당 패키지가 정상적으로 작동하는지 확인하는 test.ipynb 파일을 포함하고 있습니다.
--- utils/project.py
+++ utils/project.py
@@ -10,25 +10,35 @@ class Summary():
         self.tokenizer = AutoTokenizer.from_pretrained("wjh203/project-cp1")
         self.model = AutoModelForSeq2SeqLM.from_pretrained("wjh203/project-cp1")
         self.remove_lst = ['[\u2e80-\u2eff\u31c0-\u31ef\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fbf\uf900-\ufaff]', '(http(s)?:\/\/)([a-z0-9\w]+\.*)+[a-z0-9]{2,4}', '[a-z]+[.][a-z]+[.][a-z]+[.][a-z]', '[\u25b4\u2015]', '[⊙·■#▲▶…☎ㆍ「」─｢｣『』․ \n]', '[a-z]+[.][a-z]+[.][a-z]+[/][a-z]+', '\d+[-]\d+[-]\d+', '\(\)', '\( \)', '\]', '\(  \)']
+        self.text_lst = []
+        self.summary_lst = []
     
-    def open_txt(self, txt_file):
+    def stack_txt(self, txt_file):
         file = open(txt_file, "r")
         test_txt=file.read()
-        return test_txt
-
-    def preprocess(self, test_txt):
-        for i in self.remove_lst:
-            test_txt=re.sub(i, '', test_txt)
-
-        spacing = Spacing()
-        result = spacing(test_txt)
+        self.text_lst.append(test_txt)
         
-        return result
 
-    def inference(self, test_txt):
+    def preprocess(self):
+        
+        spacing = Spacing()
+
+        for regax in self.remove_lst:
+            for j in range(len(self.text_lst)):
+                self.text_lst[j]=re.sub(regax, '', self.text_lst[j])
+        
+        for i in range(len(self.text_lst)):
+            self.text_lst[i]=spacing(self.text_lst[i])
+
+
+    def inference(self):
         
         gen_kwargs={'length_penalty': 0.8, 'num_beams':8, 'max_length':128}
                 
         pipe = pipeline('summarization', model = self.model, tokenizer = self.tokenizer)
         
-        return pipe(test_txt, **gen_kwargs)[0]['summary_text']
+        for i in range(len(self.text_lst)):
+            summary = pipe(self.text_lst[i], **gen_kwargs)[0]['summary_text']
+            self.summary_lst.append(summary)
+        
+        return self.summary_lst
